23f98e2b4caacccae4070066ed8a802b604db409
Andrea Pepe <pepe@epigenesys.com>
2024-11-17 19:00:56 +0100

    log: test: fix - add second rpath to liker flags

--- src/module-log/test/config.mk
+++ src/module-log/test/config.mk
@@ -1,4 +1,6 @@
 TARGETNAME=test
 TARGETTYPE=exec
 INCFLAGS=-I../../../include -I../../../build/include
-LDFLAGS=-Wl,-rpath=$(shell pwd -P)/../../.. -L../../.. -L../../../build -lzoro
+LDFLAGS=-Wl,-rpath=$(shell pwd -P)/../../.. \
+		-Wl,-rpath=$(shell pwd -P)/../../../build \
+		-L../../.. -L../../../build -lzoro
